3d22944c7ea79de105066e35d666dbfb35f9e461
derui <derutakayu@gmail.com>
2020-12-16 21:04:23 +0900

    add danger file

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,1 +1,18 @@
 import {message, danger} from 'danger';
+import * as parser from 'fast-xml-parser';
+import * as fs from 'fs';
+
+
+function loadLatestDetektXml() {
+  const data= fs.readFileSync('./build/detekt/detekt.xml').toString();
+
+  const options: parser.X2jOptionsOptional = {
+    attrNodeName: "attr"
+  };
+  const obj = parser.parse(data, options)
+  console.log(obj)
+}
+
+loadLatestDetektXml()
+const modifiedMD = danger.git.modified_files.join("- ")
+message("Changed Files in this PR: \n - " + modifiedMD)
